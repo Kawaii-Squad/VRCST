f4f75ad03c68002e81f2a7afe81873bd077bfe6d
Kaichi-Sama <92471263+KaichiSama@users.noreply.github.com>
2024-03-12 08:40:46 -0400

    Update VRCST.py

--- VRCST.py
+++ VRCST.py
@@ -93,7 +93,7 @@ def get_display_name():
 # Définition du chemin local du script
 local_script_path = "VRCST.py"
 user_id_file = 'LocalDB/temps/user_id.bin'  # Nom du fichier pour enregistrer le user ID
-user_agent = 'VRC Scanner Tool / Kawaii Squad Studio'
+user_agent = 'VRST / Studio'
 auth_cookie_path = 'LocalDB/temps/AuthCookie.bin'
 friendlist = 'LocalDB/infos/friendslist.json'
 auth_cookie = get_auth_cookie(auth_cookie_path)
